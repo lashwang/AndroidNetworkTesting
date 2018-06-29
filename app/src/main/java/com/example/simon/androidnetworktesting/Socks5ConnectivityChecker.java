1ba46e7b4850c9e80a42ea61f2265884d1ebfbc8
simon <lashwang@outlook.com>
2018-06-29 11:52:56 +0800

    update

--- app/src/main/java/com/example/simon/androidnetworktesting/Socks5ConnectivityChecker.java
+++ app/src/main/java/com/example/simon/androidnetworktesting/Socks5ConnectivityChecker.java
@@ -5,10 +5,18 @@ import android.util.Log;
 
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.PasswordAuthentication;
 import java.net.Proxy;
 import java.net.Socket;
+
+import socks5lib.client.Socks5;
+import socks5lib.client.SocksProxy;
+import socks5lib.client.SocksSocket;
+
+import static socks5lib.utils.ResourceUtil.close;
 
 public class Socks5ConnectivityChecker {
 
@@ -20,15 +28,40 @@ public class Socks5ConnectivityChecker {
     private static final String user = "seven";
     private static final String password = "seven";
 
-    public void run(String url) throws Exception{
+
+    public void run(String url){
+        int length = 0;
+        byte[] buffer = new byte[2048];
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        Socket socket = null;
+
+        try {
+            SocksProxy proxy = new Socks5(
+                    new InetSocketAddress(proxyHost, proxyPort), user, password);
+            socket = new SocksSocket(proxy, new InetSocketAddress(url, 80));
+            inputStream = socket.getInputStream();
+            outputStream = socket.getOutputStream();
+            while ((length = inputStream.read(buffer)) > 0) {
+                Log.d(TAG, new String(buffer, 0, length));
+            }
+        }catch (Exception e){
+            Log.e(TAG,"get error",e);
+        }finally {
+            close(inputStream, outputStream, socket);
+        }
     }
 
     public static void check(){
-        try {
-            new Socks5ConnectivityChecker().run(url);
-        }catch (Exception e){
-            Log.d(TAG,"get error:",e);
-        }
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    new Socks5ConnectivityChecker().run(url);
+                }catch (Exception e){
+                    Log.d(TAG,"get error:",e);
+                }
+            }
+        }).start();
 
     }
 
